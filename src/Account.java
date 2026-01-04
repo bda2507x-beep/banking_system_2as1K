38a7e5c1ed3a6f6413529afa008ab7b0e5059cfa
Khanzat Akhmetov <252772@astanait.edu.kz>
2026-01-04 17:33:10 +0500

    gg

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,26 @@
 public class Account {
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+
+    public String toString() {
+        return "Account{number='" + accountNumber + "', balance=" + balance + "}";
+    }
 }
--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,43 @@
-public class Customer {
+import java.util.Objects;
+
+public class Customer extends Person {
+    private int age;
+    private Account account;
+
+    public Customer(String name, int age, Account account) {
+        super(name);
+        this.age = age;
+        this.account = account;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+
+    public String getRole() {
+        return "Customer";
+    }
+
+    public String toString() {
+        return "Customer{name='" + name +
+                "', age=" + age +
+                ", balance=" + account.getBalance() + "}";
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return name.equals(customer.name);
+    }
+
+
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
